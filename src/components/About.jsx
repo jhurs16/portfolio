fa5925671615f57c36ba330616b7c0df72650085
jhurs16 <magsayojuristthor16@gmail.com>
2023-04-13 09:40:31 +0800

    add: hh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,15 +14,12 @@ const About = ({aboutref}) => {
             {/* <small>Since 2011</small> */}
             </div>
         </div>
-        <section class="py-5 text-center container bg-light">
-    <div class="row py-2">
-      <div class="col-lg-10 col-md-10 mx-auto ">
+        <section className="py-5 text-center container bg-light">
+    <div className="row py-2">
+      <div className="col-lg-10 col-md-10 mx-auto ">
         
-        <p class="lead text-body-secondary">
+        <p className="lead text-body-secondary text-wrap">
             To become a Web Developer and learn it from scratch I risk and take my second course BS Info Tech for 4 years. My first course was not an IT related which is BS in Industrial Technology it is on designing a house or buildings, that time I dont know what my passion is, but I realized it when I was on my third year but I cant shift to Info Tech so I continue and finished my first course. After that, I worked for almost 2 years in Supermarkets to prepare and earn money for my equipment (mediums) and expenses in my school because I decided to pursue my passion in BS in Info Tech and become a programmer. After 4 years I worked as a Software Developer / Web Developer / Junior Software Engineer and learn new things on the technology today.
-        </p>
-        <p>
-
         </p>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,29 +14,29 @@ const Footer = ({contref}) => {
             <h3 className="text-uppercase custom-title mb-0 ft-wt-600 pb-3">Let's Talk</h3>
                 <p className="open-sans-font mb-3">Thank you for checking my portfolio, you can contact me below.</p>
             <div className="col-12 col-lg-4">
-                <p className="open-sans-font custom-span-contact position-relative ">
+                <div className="open-sans-font custom-span-contact position-relative ">
                     <i className="icofont-envelope position-absolute"></i>
                     <p className="mail">
                     	<span className="d-block ">mail me</span>magsayojuristthor16@gmail.com
                     </p>
-                </p>
-                <p className="open-sans-font custom-span-contact position-relative">
+                </div>
+                <div className="open-sans-font custom-span-contact position-relative">
                     <i className="icofont-ui-call position-absolute"></i>
                     <p className="phone">
                     	<span className="d-block">call me</span>+639304004150
                     </p>
-                </p>
+                </div>
             </div>
             <div className="col-12 col-lg-4">
-                <p className="open-sans-font custom-span-contact position-relative ">
+                <div className="open-sans-font custom-span-contact position-relative ">
                  
-                    <i class="icofont-linkedin position-absolute pt-2"></i>
+                    <i className="icofont-linkedin position-absolute pt-2"></i>
                     <div className="mail">
                     	<span className="d-block ">profile link</span>
                         <a href="https://www.linkedin.com/in/jurist-thor-magsayo-b7116522a" target="_blank" rel="noreferrer" className="link-info">my profile</a>
                     </div>
-                    
-                </p>
+                     
+                </div>
                 {/* <p className="open-sans-font custom-span-contact position-relative">
                     <i className="icofont-ui-call position-absolute"></i>
                     <p className="phone">
